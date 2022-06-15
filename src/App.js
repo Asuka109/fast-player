536b5d4c182d8db5b14ae96e1201358cd4aebcea
Asuka109 <1097198892@qq.com>
2022-06-15 10:57:50 +0800

    remove useEffects

--- src/App.js
+++ src/App.js
@@ -1,48 +1,84 @@
 import "./styles.css";
-import { useThrottleFn, usePrevious } from "ahooks";
-import { useState, useRef, useEffect } from "react";
+import { useInterval, useThrottleFn } from "ahooks";
+import { useState, useRef } from "react";
 
-const Video = () => {
-  const videoRef = useRef(null);
-  const onTick = () => {}; //console.log(videoRef.current?.currentTime);
-
-  const [isPlaying, setPlaying] = useState(false);
-  const freshPlayingState = () => setPlaying(!videoRef.current?.paused);
-  const previousTime = useRef(-1);
-  const isChanged = () =>
-    previousTime.current !== videoRef.current?.currentTime;
-  const _fireTickEvent = useThrottleFn(onTick, { wait: 32 });
-  const fireTickEvent = () => {
-    const changed = isChanged();
-    if (changed) {
-      console.log(
-        (videoRef.current?.currentTime - previousTime.current) * 1000
-      );
-      _fireTickEvent.run();
+const useVideoTick = ({ ref, handler, rate }) => {
+  const fireTickEvent = useThrottleFn(handler, { wait: rate });
+  const reqAnimLock = useRef(0);
+  const freshPlayingState = () => {
+    cancelAnimationFrame(reqAnimLock.current);
+    if (ref.current && !ref.current.paused) {
+      const cb = () => {
+        fireTickEvent.run();
+        reqAnimLock.current = requestAnimationFrame(cb);
+      };
+      cb();
     }
-    previousTime.current = videoRef.current?.currentTime;
   };
-  useEffect(() => {
-    if (isPlaying) {
-      const timer = setInterval(fireTickEvent, 8);
-      return () => clearInterval(timer);
+  return {
+    onPlaying: freshPlayingState,
+    onPause: freshPlayingState,
+    onSeeking: fireTickEvent.run
+  };
+};
+
+const composeEventProps = (...propsSet) => {
+  const final = {};
+  for (const props of propsSet) {
+    for (const [event, handler] of Object.entries(props)) {
+      final[event] = final[event] || [];
+      final[event].push(handler);
     }
-  }, [isPlaying]);
+  }
+  for (const event of Object.keys(final)) {
+    const handlers = final[event];
+    final[event] = (...args) => handlers.forEach((handler) => handler(...args));
+  }
+  return final;
+};
+
+const Video = (props) => {
+  const videoRef = useRef(null);
+  const videoTickProps = useVideoTick({
+    ref: videoRef,
+    handler: () => props.onTick?.(videoRef.current),
+    rate: 1000 / 60
+  });
+  const finalProps = composeEventProps(videoTickProps, {
+    onPlaying: () => console.log(123)
+  });
   return (
     <video
       src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
       ref={videoRef}
-      autoPlay
+      autoPlay={false}
       controls
-      onPlaying={freshPlayingState}
-      onPause={freshPlayingState}
-      onSeeking={fireTickEvent}
+      {...finalProps}
     />
   );
 };
 
 const App = () => {
-  return <Video />;
+  const [count, setCount] = useState(0);
+  const [rate, setRate] = useState(0);
+  const ticksEachSec = useRef(0);
+  useInterval(() => {
+    setRate(ticksEachSec.current);
+    ticksEachSec.current = 0;
+  }, 1000);
+  return (
+    <div>
+      <Video
+        onTick={(video) => {
+          setCount(count + 1);
+          ticksEachSec.current++;
+          video.currentTime > 3 && (video.currentTime = 0);
+        }}
+      />
+      <span>{count}</span>
+      <p>{rate}</p>
+    </div>
+  );
 };
 
 export default App;
